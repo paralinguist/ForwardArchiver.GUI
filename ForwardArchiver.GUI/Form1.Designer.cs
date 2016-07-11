e2811560cf14b49373664fbef4be220d329790f5
paralinguist <jonathan@ihle.in>
2016-07-11 18:44:24 +0800

    Initial Commit

---
+++ ForwardArchiver.GUI/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace ForwardArchiver.GUI
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.archive_mail = new System.Windows.Forms.Button();
+            this.feedback_log = new System.Windows.Forms.TextBox();
+            this.department_email = new System.Windows.Forms.TextBox();
+            this.dept_email_label = new System.Windows.Forms.Label();
+            this.password_label = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.gmail_label = new System.Windows.Forms.Label();
+            this.gmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.exchange_URL = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // archive_mail
+            // 
+            this.archive_mail.Enabled = false;
+            this.archive_mail.Location = new System.Drawing.Point(15, 124);
+            this.archive_mail.Name = "archive_mail";
+            this.archive_mail.Size = new System.Drawing.Size(413, 23);
+            this.archive_mail.TabIndex = 5;
+            this.archive_mail.Text = "Enter Password Above";
+            this.archive_mail.UseVisualStyleBackColor = true;
+            this.archive_mail.Click += new System.EventHandler(this.archive_mail_Click);
+            // 
+            // feedback_log
+            // 
+            this.feedback_log.Enabled = false;
+            this.feedback_log.Location = new System.Drawing.Point(15, 162);
+            this.feedback_log.Multiline = true;
+            this.feedback_log.Name = "feedback_log";
+            this.feedback_log.ReadOnly = true;
+            this.feedback_log.Size = new System.Drawing.Size(413, 137);
+            this.feedback_log.TabIndex = 1;
+            // 
+            // department_email
+            // 
+            this.department_email.Location = new System.Drawing.Point(136, 32);
+            this.department_email.Name = "department_email";
+            this.department_email.Size = new System.Drawing.Size(292, 20);
+            this.department_email.TabIndex = 2;
+            // 
+            // dept_email_label
+            // 
+            this.dept_email_label.AutoSize = true;
+            this.dept_email_label.Location = new System.Drawing.Point(12, 35);
+            this.dept_email_label.Name = "dept_email_label";
+            this.dept_email_label.Size = new System.Drawing.Size(93, 13);
+            this.dept_email_label.TabIndex = 3;
+            this.dept_email_label.Text = "Department Email:";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Location = new System.Drawing.Point(12, 58);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(114, 13);
+            this.password_label.TabIndex = 4;
+            this.password_label.Text = "Department Password:";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(136, 58);
+            this.password.Name = "password";
+            this.password.PasswordChar = '•';
+            this.password.Size = new System.Drawing.Size(133, 20);
+            this.password.TabIndex = 3;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // gmail_label
+            // 
+            this.gmail_label.AutoSize = true;
+            this.gmail_label.Location = new System.Drawing.Point(12, 9);
+            this.gmail_label.Name = "gmail_label";
+            this.gmail_label.Size = new System.Drawing.Size(77, 13);
+            this.gmail_label.TabIndex = 7;
+            this.gmail_label.Text = "Gmail Address:";
+            // 
+            // gmail
+            // 
+            this.gmail.Location = new System.Drawing.Point(136, 6);
+            this.gmail.Name = "gmail";
+            this.gmail.Size = new System.Drawing.Size(292, 20);
+            this.gmail.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Exchange WS URL:";
+            // 
+            // exchange_URL
+            // 
+            this.exchange_URL.Location = new System.Drawing.Point(136, 84);
+            this.exchange_URL.Name = "exchange_URL";
+            this.exchange_URL.Size = new System.Drawing.Size(292, 20);
+            this.exchange_URL.TabIndex = 4;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 313);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exchange_URL);
+            this.Controls.Add(this.gmail_label);
+            this.Controls.Add(this.gmail);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.dept_email_label);
+            this.Controls.Add(this.department_email);
+            this.Controls.Add(this.feedback_log);
+            this.Controls.Add(this.archive_mail);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "ForwardArchiver";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmMain_FormClosed);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button archive_mail;
+        private System.Windows.Forms.TextBox feedback_log;
+        private System.Windows.Forms.TextBox department_email;
+        private System.Windows.Forms.Label dept_email_label;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label gmail_label;
+        private System.Windows.Forms.TextBox gmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox exchange_URL;
+    }
+}
+
---
+++ ForwardArchiver.GUI/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using Microsoft.Exchange.WebServices.Data;
+
+namespace ForwardArchiver.GUI
+{
+    public partial class FrmMain : Form
+    {
+        public String settings_file = "fwdarch.json";
+        public  String category = "Purple Category";
+        public  int max_returned = 999;
+        public  int received_backups = 0;
+        public  int sent_backups = 0;
+        public  int auth_server_fails = 0;
+        public  bool archive_complete = false;
+        public  bool needs_repeat = false;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void archive_mail_Click(object sender, EventArgs e)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = CertificateValidationCallBack;
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2013_SP1);
+            service.Credentials = new WebCredentials(department_email.Text, password.Text);
+            service.Url = new Uri(exchange_URL.Text);
+
+            FolderId inbox = WellKnownFolderName.Inbox;
+            FolderId sent = WellKnownFolderName.SentItems;
+
+            while (!archive_complete)
+            {
+                needs_repeat = false;
+                FindItemsResults<Item> unarchived_mail = FindMail(inbox, service);
+                if (unarchived_mail != null)
+                {
+                    received_backups = ForwardMail(unarchived_mail, service, "received");
+                }
+                else
+                {
+                    needs_repeat = true;
+                }
+
+                unarchived_mail = FindMail(sent, service);
+                if (unarchived_mail != null)
+                {
+                    sent_backups = ForwardMail(unarchived_mail, service, "sent");
+                }
+                else
+                {
+                    needs_repeat = true;
+                }
+
+                feedback_log.Text += "Received mail backed up: " + received_backups + "\r\n";
+                feedback_log.Text += "Sent mail backed up: " + sent_backups + "\r\n";
+                feedback_log.Text += "Auth server fails: " + auth_server_fails + "\r\n";
+                if (needs_repeat)
+                {
+                    archive_complete = false;
+                    feedback_log.Text += "Possibly missed some mail last time - re-running.\r\n";
+                }
+                else
+                {
+                    archive_complete = true;
+                }
+            }
+            feedback_log.Text += "Done\r\n";
+        }
+
+        private FindItemsResults<Item> FindMail(FolderId location, ExchangeService service)
+        {
+            FindItemsResults<Item> unarchived_mail = null;
+            try
+            {
+                SearchFilter.IsNotEqualTo category_test = new SearchFilter.IsNotEqualTo(EmailMessageSchema.Categories, category);
+                SearchFilter sf = new SearchFilter.SearchFilterCollection(LogicalOperator.And, category_test);
+                ItemView view = new ItemView(max_returned);
+                unarchived_mail = service.FindItems(location, sf, view);
+            }
+            catch
+            {
+                feedback_log.Text += "Auth server fail\r\n";
+                auth_server_fails++;
+
+            }
+            return unarchived_mail;
+        }
+
+        private int ForwardMail(FindItemsResults<Item> unarchived_mail, ExchangeService service, String tag)
+        {
+            int successful_forwards = 0;
+            foreach (Item item in unarchived_mail)
+            {
+                try
+                {
+                    EmailMessage message = EmailMessage.Bind(service, item.Id);
+                    ResponseMessage responseMessage = message.CreateForward();
+                    responseMessage.ToRecipients.Add(gmail.Text);
+                    responseMessage.BodyPrefix = "Archived:" + tag;
+                    responseMessage.Send();
+                    message.Categories.Add(category);
+                    if (!message.IsRead)
+                    {
+                        message.IsRead = false;
+                    }
+                    message.Update(ConflictResolutionMode.AutoResolve, true);
+                    successful_forwards++;
+                }
+                catch
+                {
+                    feedback_log.Text += "Auth server fail\r\n";
+                    auth_server_fails++;
+                }
+            }
+            return successful_forwards;
+        }
+
+        //The below functions are stolen wholesale from MSDN examples
+        private bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
+
+            Uri redirectionUri = new Uri(redirectionUrl);
+
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        private bool CertificateValidationCallBack(
+                            object sender,
+                            System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+                            System.Security.Cryptography.X509Certificates.X509Chain chain,
+                            System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            // If the certificate is a valid, signed certificate, return true.
+            if (sslPolicyErrors == System.Net.Security.SslPolicyErrors.None)
+            {
+                return true;
+            }
+
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if ((sslPolicyErrors & System.Net.Security.SslPolicyErrors.RemoteCertificateChainErrors) != 0)
+            {
+                if (chain != null && chain.ChainStatus != null)
+                {
+                    foreach (System.Security.Cryptography.X509Certificates.X509ChainStatus status in chain.ChainStatus)
+                    {
+                        if ((certificate.Subject == certificate.Issuer) &&
+                           (status.Status == System.Security.Cryptography.X509Certificates.X509ChainStatusFlags.UntrustedRoot))
+                        {
+                            // Self-signed certificates with an untrusted root are valid. 
+                            continue;
+                        }
+                        else
+                        {
+                            if (status.Status != System.Security.Cryptography.X509Certificates.X509ChainStatusFlags.NoError)
+                            {
+                                // If there are any other errors in the certificate chain, the certificate is invalid,
+                                // so the method returns false.
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                // When processing reaches this line, the only errors in the certificate chain are 
+                // untrusted root errors for self-signed certificates. These certificates are valid
+                // for default Exchange server installations, so return true.
+                return true;
+            }
+            else
+            {
+                // In all other cases, return false.
+                return false;
+            }
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            FwdArchSettings settings = FwdArchSettings.Load(settings_file);
+            department_email.Text = settings.exchange_address;
+            gmail.Text = settings.gmail_address;
+            exchange_URL.Text = settings.ews_url.ToString();
+        }
+
+        private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            FwdArchSettings settings = FwdArchSettings.Load(settings_file);
+            settings.exchange_address = department_email.Text;
+            settings.gmail_address = gmail.Text;
+            settings.ews_url = new Uri(exchange_URL.Text);
+            settings.Save(settings_file);
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+            if (password.Text.Length > 0)
+            {
+                archive_mail.Enabled = true;
+            }
+            else
+            {
+                archive_mail.Enabled = false;
+            }
+        }
+    }
+
+    class FwdArchSettings : AppSettings<FwdArchSettings>
+    {
+        public String gmail_address = "firstname.surname@ashdalesc.wa.edu.au";
+        public String exchange_address = "firstname.surname@education.wa.edu.au";
+        public Uri ews_url = new Uri(@"https://webmail.det.wa.edu.au/EWS/Exchange.asmx");
+        public String category = "Purple Category";
+        public int max_returned = 999;
+    }
+
+    //Settings based on this excellent SO answer:
+    //http://stackoverflow.com/a/6541739
+    public class AppSettings<T> where T : new()
+    {
+        public void Save(string fileName)
+        {
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(this));
+        }
+
+        public static void Save(T pSettings, string fileName)
+        {
+            File.WriteAllText(fileName, (new JavaScriptSerializer()).Serialize(pSettings));
+        }
+
+        public static T Load(string fileName)
+        {
+            T t = new T();
+            if (File.Exists(fileName))
+                t = (new JavaScriptSerializer()).Deserialize<T>(File.ReadAllText(fileName));
+            return t;
+        }
+    }
+}
+//JDI: Assault (Madcap)
